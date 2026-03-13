118a5b29e3bfddcc8de58499b7aee0ca9411db0c
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-03-13 16:08:41 -0300

    add finish

--- Q1_Dobro.asm
+++ Q1_Dobro.asm
@@ -3,15 +3,19 @@
 .text
 
 input:	
-	addi $v0, $zero, 5 #@v0/$2 = $zero + 5
+	addi $v0, $zero, 5 #@v0 = $zero + 5
 	syscall
 	add $t0, $zero, $v0 #mover valor de $v0 para o reg t0
 	
 calc:	
 	addi $t1, $zero, 2 # armazenei o valor 2 no reg t1 para multiplicar	
-	mul $t2, $t0, $t1 #t3 armazena o valor de t1*t2
+	mul $t2, $t0, $t1 #t3 armazena o valor de t0*t1
 	
 output:
-	addi $v0, $zero, 1 #se v0 = 1 printa inteiro
-	add $a0, $zero, $t2 #a0 recebe o valor de t3 para poder imprimir
+	addi $v0, $zero, 1 #como v0 = 1 printa inteiro
+	add $a0, $zero, $t2 #a0 recebe o valor de t2 para imprimir
+	syscall
+	
+finish:
+	addi $v0, $zero, 10
 	syscall
